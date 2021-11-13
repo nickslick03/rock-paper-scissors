a068027e0fb5bbe1ce2f0bb3275e0f468b8373df
nickslick03 <nicholasepps03@gmail.com>
2021-11-13 16:00:06 -0500

    script

---
+++ script.js
@@ -0,0 +1,71 @@
+function computerSelection() {
+    let num=Math.floor(Math.random()*3);
+    if(num==0) {
+        return "rock";
+    } else if(num==1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+function playerSelection() {
+    let answer=prompt("rock, paper, or scissors?");
+    answer=answer.toLowerCase();
+    while(answer!="rock"&&answer!="paper"&&answer!="scissors") {
+        alert("that's not rock, paper, or scissors!")
+        answer=prompt("rock, paper, or scissors?");
+        answer=answer.toLowerCase();
+    }
+    return answer;
+}
+function playRound(computer,player) 
+{
+    if(player=="rock") 
+    {
+        if(computer=="rock") 
+        {
+            alert("Tie! rock and rock.");
+        } 
+        else if(computer=="paper") 
+        {
+            alert("You lose! paper beats rock.");
+        } else 
+        {
+            alert("You win! rock beats scissors.");
+        }
+    } 
+    else if(player=="paper") 
+    {
+        if(computer=="rock") 
+        {
+            alert("You win! paper beats rock.");
+        } 
+        else if(computer=="paper") 
+        {
+            alert("Tie. paper and paper.");
+        } 
+        else 
+        {
+            alert("You lose! scissor beats paper.");
+        }
+    } 
+    else
+    {
+        if(computer=="rock") 
+        {
+            alert("You lose! rock beats scissors.");
+        } 
+        else if(computer=="paper") 
+        {
+            alert("You win! scissors beats paper.");
+        } 
+        else 
+        {
+            alert("Tie! scissors and scissors.");
+        }
+    }   
+}
+for(let i=0;i<5;i++)
+{
+    playRound(computerSelection(),playerSelection());
+}
